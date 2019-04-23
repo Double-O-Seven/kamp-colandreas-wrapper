540c9bfb26866893f68d31f3960caa1d695fd877
leadrian <thewishwithin@gmail.com>
2019-04-23 21:25:13 +0200

    Get plugin-wrapper-generator from Gradle Repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,5 @@
-import ch.leadrian.samp.kamp.gradle.plugin.pluginwrappergenerator.PluginWrapperGeneratorExtension
 import com.google.common.base.CaseFormat
 import groovy.lang.Closure
-
-buildscript {
-    dependencies {
-        repositories {
-            mavenCentral()
-            mavenLocal()
-            maven {
-                setUrl("https://plugins.gradle.org/m2/")
-            }
-        }
-
-        classpath(
-                group = "ch.leadrian.samp.kamp",
-                name = "kamp-plugin-wrapper-generator",
-                version = "1.0.0-rc2"
-        )
-    }
-}
 
 plugins {
     kotlin("jvm") version "1.3.11"
@@ -31,29 +12,14 @@ plugins {
     jacoco
     id("org.jetbrains.dokka") version "0.9.17"
     id("com.palantir.git-version") version "0.12.0-rc2"
+    id("ch.leadrian.samp.kamp.kamp-plugin-wrapper-generator") version "1.0.0-rc2"
 }
-
-apply(plugin = "kamp-plugin-wrapper-generator")
 
 repositories {
     mavenCentral()
-    mavenLocal()
     maven {
         setUrl("https://dl.bintray.com/spekframework/spek")
     }
-}
-
-jacoco {
-    toolVersion = "0.8.3"
-}
-
-configure<PluginWrapperGeneratorExtension> {
-    packageName = "ch.leadrian.samp.kamp.colandreaswrapper"
-    pluginName = "ColAndreas"
-    removePrefix("CA_")
-    nativeFunctionsCaseFormat = CaseFormat.UPPER_CAMEL
-    callbacksCaseFormat = CaseFormat.UPPER_CAMEL
-    interfaceDefintionFile(project.projectDir.resolve("src/main/idl/ColAndreas.idl"))
 }
 
 dependencies {
@@ -126,6 +92,19 @@ tasks {
     dokka {
         reportUndocumented = false
     }
+}
+
+jacoco {
+    toolVersion = "0.8.3"
+}
+
+pluginWrapperGenerator {
+    packageName = "ch.leadrian.samp.kamp.colandreaswrapper"
+    pluginName = "ColAndreas"
+    removePrefix("CA_")
+    nativeFunctionsCaseFormat = CaseFormat.UPPER_CAMEL
+    callbacksCaseFormat = CaseFormat.UPPER_CAMEL
+    interfaceDefintionFile(project.projectDir.resolve("src/main/idl/ColAndreas.idl"))
 }
 
 publishing {
